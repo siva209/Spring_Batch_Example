bbed2176ddf3b4b6b2ba218e36f7ef5bfe9f721b
siva209 <sivanarayana209@gmail.com>
2021-07-22 19:05:12 +0530

    Added:SpringBatch read data from excel file and stored in database

---
+++ src/main/java/com/demo/springbatch/SpringBatchExampleApplication.java
@@ -0,0 +1,14 @@
+package com.demo.springbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchExampleApplication.class, args);
+		System.out.println("siva");
+	}
+
+}
